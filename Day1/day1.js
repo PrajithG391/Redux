122ebd294b679c0f9ff9f47028dd5c972d6fd788
Prajith G <kuttanthampuran3669@gmail.com>
2025-07-18 16:37:34 +0530

    day1.js

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,7 +1,7 @@
-// 1️⃣ Initial State
+
 const initialState = { count: 0 };
 
-// 2️⃣ Reducer (chef)
+
 function counterReducer(state = initialState, action) {
   switch (action.type) {
     case "INCREMENT":
@@ -13,15 +13,15 @@ function counterReducer(state = initialState, action) {
   }
 }
 
-// 3️⃣ Create Store (warehouse)
+
 const store = Redux.createStore(counterReducer);
 
-// 4️⃣ Subscribe → whenever store updates, update the UI
+
 store.subscribe(() => {
   document.getElementById("count").innerText = store.getState().count;
 });
 
-// 5️⃣ Dispatch Actions (order slips)
+
 function increment() {
   store.dispatch({ type: "INCREMENT" });
 }
